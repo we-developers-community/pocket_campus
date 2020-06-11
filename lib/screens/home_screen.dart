bd9641486ac037caf23347c3cf65f647547e9999
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-06-11 19:06:48 +0530

    added placeholder in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,19 +2,52 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home';
+
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-    final theme = Theme.of(context);
-    
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Pocket Campus'),
       ),
       body: Container(
+        height: mediaQuery.size.height * 0.9,
+        width: mediaQuery.size.width,
         child: Column(
           children: <Widget>[
-            
+            Container(
+              height: mediaQuery.size.height * 0.28,
+              width: mediaQuery.size.width,
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade800,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: null,
+            ),
+            Container(
+              height: mediaQuery.size.height * 0.35,
+              width: mediaQuery.size.width,
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: GridView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                itemCount: 6,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1 / 1,
+                  crossAxisSpacing: 30,
+                  mainAxisSpacing: 30,
+                ),
+                itemBuilder: (ctx, item) => Container(
+                  child: Text(''),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade800,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
