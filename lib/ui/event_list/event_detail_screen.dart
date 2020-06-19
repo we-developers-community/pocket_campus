91d90994588ff990ea4dd4a05fb1d3ea75f99c88
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-06-20 00:18:18 +0530

    added event_detail_screen

---
+++ lib/ui/event_list/event_detail_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class EventDetailScreen extends StatelessWidget {
+  static const routeName = '/event-detail';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // appBar: AppBar(
+        //    title: Text(''),
+        // ),
+        body: CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          expandedHeight: 300,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(''),
+            background: Hero(
+              tag: '',
+              child: Image.network(
+                '',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              Container(
+                height: 300,
+                width: 300,
+                child: Text('description'),
+              )
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
