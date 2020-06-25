473065d25d6802949f634916b83da5f0f16d9ae2
Suvansh Rana <36293610+suvansh-rana@users.noreply.github.com>
2020-06-25 18:02:03 +0530

    Tweaked Campus Map

--- lib/ui/campus_map/campus_map.dart
+++ lib/ui/campus_map/campus_map.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class CampusMapScreen extends StatelessWidget {
+class CampusMapScreen extends StatefulWidget {
 
   static final String routeName = "/campus-map";
+
+  @override
+  _CampusMapScreenState createState() => _CampusMapScreenState();
+}
+
+class _CampusMapScreenState extends State<CampusMapScreen> {
+
+  WebViewController _controller;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +22,19 @@ class CampusMapScreen extends StatelessWidget {
       body: Builder(
         builder: (BuildContext context) {
           return WebView(
-            initialUrl: 'https://www.google.com/maps/place/Shri+Shankaracharya+Group+of+Institutions+(S1)/@21.2161967,81.3055806,15z/data=!4m12!1m6!3m5!1s0x3a293d3d85555555:0x2d082fae1d006e97!2sShri+Shankaracharya+Group+of+Institutions+(S1)!8m2!3d21.2161917!4d81.3077693!3m4!1s0x3a293d3d85555555:0x2d082fae1d006e97!8m2!3d21.2161917!4d81.3077693',
+            onWebViewCreated: (WebViewController controller) {
+              setState(() {
+                _controller = controller;
+              });
+            },
+            onPageFinished: (String url) {
+ 
+              // Manipulating DOM to remove searchbox and FAB
+
+              _controller.evaluateJavascript("document.querySelector('.ml-searchbox-button-parent').remove();document.querySelector('.ml-fab-inner').remove();var map = document.querySelector('.map'); map.style.transform='none'; map.style.maxHeight='100%';");
+            },
+            initialUrl:
+                'https://www.google.com/maps/place/Shri+Shankaracharya+Group+of+Institutions+(S1),+Junwani,+Chhattisgarh+490020/@21.2148189,81.3084021,17z/data=!4m2!3m1!1s0x3a293d3d85555555:0x2d082fae1d006e97',
             javascriptMode: JavascriptMode.unrestricted,
             gestureNavigationEnabled: true,
           );
